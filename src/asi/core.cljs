c1094f3b349c3482c8fdabc4d7011f0425517e80
Rob Ashton <robashton@codeofrob.com>
2013-11-09 15:11:45 +0000

    So I could have it so entities are just their own process and their own loop

--- src/asi/core.cljs
+++ src/asi/core.cljs
@@ -14,48 +14,49 @@
   (set! (. ctx -fillStyle) c)
   (.fillRect ctx x y w h))
 
-(defn enemy-action [state action]
-  (case (:cmd action)
-    :render (do (fill-rect (:ctx action) state "#FF0") state)
-    :tick state))
-
-(defn player-action [state action]
-  (case (:cmd action)
-    :render (do (fill-rect (:ctx action) state "#F00") state)
-    :tick state))
-
 (defn enemy [out x y w h]
  (let [in (chan)]
    (go (loop [state {:x x :y y :w w :h h}]
-        (recur (enemy-action state (<! in)))))
-   in))
+         (if-let [action (<! in)]
+           (recur (case (:cmd action)
+            :render (do (fill-rect (:ctx action) state "#FF0") state)
+            :tick state)))))
+   {:channel in :id (str "enemy" x y) }))
 
 (defn player [out x y w h]
  (let [in (chan)]
    (go (loop [state {:x x :y y :w w :h h}]
-        (recur (player-action state (<! in)))))
-   in))
+         (if-let [action (<! in)]
+           (recur (case (:cmd action)
+            :render (do (fill-rect (:ctx action) state "#F00") state)
+            :tick state)))))
+   {:channel in :id "player"}))
+
 
 (defn initial-state
   [out ctx]
-  { :entities (conj
-                (for [x (range 0 480 60)
-                    y (range 0 240 60)]
-                  (enemy out x y 20 20))
-                (player out 200 430 20 20))
-    :ctx ctx }) 
+  { :entities (into {} 
+                (for [e 
+                  (conj (for [x (range 0 480 60)
+                        y (range 0 240 60)]
+                      (enemy out x y 20 20))
+                        (player out 200 430 20 20))] [(:id e) e]))
+    :ctx ctx })
+
+(defn broadcast [scene cmd & data]
+  (doseq [[id {ch :channel}] (:entities scene)] (put! ch (assoc (apply hash-map data) :cmd cmd)))
+  scene)
 
 (defn game-action 
-  [state action]
+  [scene action]
   (case action
-    :tick (doseq [e (:entities state)] (put! e { :cmd :tick}))
-    :render (doseq [e (:entities state)] (put! e { :cmd :render :ctx (:ctx state)}))))
+    :tick (broadcast scene :tick)
+    :render (broadcast scene :render :ctx (:ctx scene))))
 
 (defn game
   [in ctx]
   (go (loop [state (initial-state in ctx)] 
-        (game-action state (<! in))
-        (recur state))))
+        (recur (game-action state (<! in))))))
 
 (defn start 
   []
