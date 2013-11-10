ce76dd9869ad19b93fbb93d8dfc033fe3d8a2708
Rob Ashton <robashton@codeofrob.com>
2013-11-10 08:24:21 +0000

    How about using the channel as a means of scheduling and scheduling only

--- src/asi/core.cljs
+++ src/asi/core.cljs
@@ -14,78 +14,65 @@
   (set! (. ctx -fillStyle) "#FFF")
   (.fillRect ctx 0 0 640 480))
 
-(defn fill-rect [ctx {:keys [x y w h]} c]
-  (set! (. ctx -fillStyle) c)
-  (.fillRect ctx x y w h))
+(defn draw-entity [ctx {:keys [bounds color]}]
+  (set! (. ctx -fillStyle) color)
+  (.fillRect ctx (:x bounds) (:y bounds) (:w bounds) (:h bounds)))
 
-(defn enemy [out x y w h]
- (let [in (chan)]
-   (go (loop [state {:x x :y y :w w :h h}]
-         (if-let [action (<! in)]
-           (recur (case (:cmd action)
-            :render (do (fill-rect (:ctx action) state "#FF0") state)
-            :tick state)))))
-   {:channel in :id (str "enemy" x y) }))
+(defn rect [x y w h] { :x x :y y :w w :h h})
 
-(defn player [out x y w h]
- (let [in (chan)]
-   (go (loop [state {:x x :y y :w w :h h }]
-         (if-let [msg (<! in)]
-           (recur (case (:cmd msg)
-            :render (do (fill-rect (:ctx msg) state "#F00") state)
-            :change-dir (assoc state :dir (:dir msg))
-            :tick (case (:dir state)
-                    :left (update-in state [:x] dec)
-                    :right (update-in state [:x] inc)
-                    state))))))
-   {:channel in :id :player}))
+(defn entity [id rect color]
+  (-> {}
+    (assoc :bounds rect)
+    (assoc :color color)
+    (assoc :id id)))
 
-(defn initial-state
+(defn enemy [x y w h]
+  (entity (str "enemy" x y) (rect x y w h) "#FF0"))
+
+(defn player [x y w h]
+  (entity :player (rect x y w h) "#F00"))
+
+(defn player-left [scene]
+  (assoc-in scene [:entities :player :velx] -1))
+
+(defn player-right [scene]
+  (assoc-in scene [:entities :player :velx] 1))
+
+(defn player-halt [scene]
+  (assoc-in scene [:entities :player :velx] 0))
+
+(defn initial-scene
   [out ctx]
   { :entities (into {} 
                 (for [e 
-                  (conj (for [x (range 0 480 60)
-                        y (range 0 240 60)]
-                      (enemy out x y 20 20))
-                        (player out 200 430 20 20))] [(:id e) e]))
-    :ctx ctx })
+                  (conj (for [x (range 0 480 60) y (range 0 240 60)]
+                             (enemy x y 20 20))
+                        (player 200 430 20 20))] [(:id e) e])) })
 
-(defn broadcast 
-  [scene msg]
-  (doseq [[id {ch :channel}] (:entities scene)] (put! ch msg))
-  scene)
+(defn tick [{:keys [entities] :as scene}]
+  (assoc scene :entities (map #(assoc %1 2 (tick (val %1))) entities)))
 
-(defn message
-  [scene id msg]
-  (if-let [ch (get-in scene [:entities id :channel])] (put! ch msg))
-  scene)
-
-(defn game-action 
-  [scene msg]
-  (if-let [id (:id msg)]
-    (message scene id msg)
-    (broadcast scene msg)))
+(defn render [ctx {:keys [entities]}]
+  (clear ctx)
+  (doseq [e (map val entities)] (draw-entity ctx e)))
 
 (defn game
   [in ctx]
-  (go (loop [state (initial-state in ctx)] 
-        (recur (game-action state (<! in))))))
-
-(defn send-to 
-  [port cmd & data]
-  (put! port (assoc (apply hash-map data) :cmd cmd)))
+  (go (loop [scene (initial-scene in ctx)] 
+        (if-let [action (<! in)]
+          (recur (or (action scene) scene))))))
 
 (defn on-key-down [commands]
   (fn [e] 
     (case (. e -keyCode)
-      37 (send-to commands :change-dir :id :player :dir :left)
-      39 (send-to commands :change-dir :id :player :dir :right))))
+      37 (put! commands player-left)
+      39 (put! commands player-right))))
 
 (defn on-key-up [commands]
   (fn [e] 
     (case (. e -keyCode)
-      37 (send-to commands :change-dir :id :player :dir :none) 
-      39 (send-to commands :change-dir :id :player :dir :none))))
+      37 (put! commands player-halt)
+      39 (put! commands player-halt))))
 
 (defn hook-input-events [commands]
   (.addEventListener js/document "keydown" (on-key-down commands))
@@ -96,7 +83,7 @@
         commands (chan)
         instance (game commands ctx)]
    (hook-input-events commands)
-   (js/setInterval #(send-to commands :tick) (/ 1000.0 30.0)) 
-   (js/setInterval #(do (clear ctx) (send-to commands :render :ctx ctx)) (/ 1000.0 30.0))))
+   (js/setInterval (put! commands tick) (/ 1000.0 30.0)) 
+    (js/setInterval #(put! commands (partial render ctx)) (/ 1000.0 30.0))))
 
 (start)
